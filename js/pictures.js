0fbc3c708935ec211a769e62f99a7c08ff93b0bc
nitrousgarage <nitrousgarage90@gmail.com>
2022-10-24 12:43:08 +0300

    Добавляет дз.7

---
+++ js/pictures.js
@@ -0,0 +1,20 @@
+import {getPhotos} from './data.js';
+
+const picturesContainer = document.querySelector ('.pictures');
+const pictureTemplate = document.querySelector ('#picture').content.querySelector('.picture');
+
+const photos = getPhotos();
+
+const photoFragment = document.createDocumentFragment();
+
+photos.forEach(({url, comments, likes}) => {
+const photoItem = pictureTemplate.cloneNode(true);
+photoItem.querySelector('.picture__img').src = url;
+photoItem.querySelector('.picture__comments').textContent = comments;
+photoItem.querySelector('.picture__likes').textContent = likes;
+photoFragment.append(photoItem);
+});
+
+const getPictures = () => {picturesContainer.append(photoFragment)};
+
+export {getPictures};
