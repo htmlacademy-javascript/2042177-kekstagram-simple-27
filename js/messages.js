bf99eea77504566029c692327bc482d5f1abeb80
nitrousgarage <nitrousgarage90@gmail.com>
2022-11-23 12:57:19 +0300

    Вносит правки по критериям Д4. и Д6.

--- js/messages.js
+++ js/messages.js
@@ -3,14 +3,14 @@ import { isEscapeKey } from './utils.js';
 const ERROR_SHOW_TIME = 5000;
 
 const showMessage = (type) => {
-  const template = document.querySelector(`#${type}`).content.querySelector(`.${type}`);
-  const message = template.cloneNode(true);
-  const messageCloseButton = message.querySelector(`.${type}__button`);
+  const templateElement = document.querySelector(`#${type}`).content.querySelector(`.${type}`);
+  const messageElement = templateElement.cloneNode(true);
+  const messageCloseButton = messageElement.querySelector(`.${type}__button`);
   const closeMessage = () => {
     document.removeEventListener('keydown', onKeydown);
-    message.removeEventListener('click', onMessageClick);
-    messageCloseButton.removeEventListener('click', closeMessage);
-    document.body.removeChild(message);
+    messageElement.removeEventListener('click', onMessageClick);
+    messageCloseButton.removeEventListener('click', onCloseButtonClick);
+    document.body.removeChild(messageElement);
   };
 
   // Использовал Function Declaration поскольку использовал раньше чем объявляется!
@@ -21,21 +21,24 @@ const showMessage = (type) => {
     }
   }
   function onMessageClick(evt) {
-    if (evt.target === message) {
+    if (evt.target === messageElement) {
       closeMessage();
     }
   }
+  function onCloseButtonClick() {
+    closeMessage();
+  }
 
   document.addEventListener('keydown', onKeydown);
-  message.addEventListener('click', onMessageClick);
-  messageCloseButton.addEventListener('click', closeMessage);
-  document.body.append(message);
+  messageElement.addEventListener('click', onMessageClick);
+  messageCloseButton.addEventListener('click', onCloseButtonClick);
+  document.body.append(messageElement);
 };
 
 const showSuccessMessage = () => showMessage('success');
 const showErrorMessage = () => showMessage('error');
 
-const showDataLoadError = (message) => {
+const showDataLoadError = (messageElement) => {
   const mistakeElement = document.createElement('div');
   mistakeElement.style.zIndex = '100';
   mistakeElement.style.position = 'absolute';
@@ -47,7 +50,7 @@ const showDataLoadError = (message) => {
   mistakeElement.style.textAlign = 'center';
   mistakeElement.style.backgroundColor = 'red';
 
-  mistakeElement.textContent = message;
+  mistakeElement.textContent = messageElement;
 
   document.body.append(mistakeElement);
 
